cf05610c107dc18e9d74f1fcd3212c7f3d5c16b9
Laval <lavalojan.14@cse.mrt.ac.lk>
2018-09-10 04:50:47 +0530

    create components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { AboutComponent } from './about/about.component';
+import { ProductListComponent } from './products/product-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutComponent,
+    ProductListComponent
   ],
   imports: [
     BrowserModule
